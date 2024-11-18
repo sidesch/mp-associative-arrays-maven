f9c2c9057a4192624cca55b5b7082f99e2663b7a
sarah_deschamps <deschamps.sias@gmail.com>
2024-11-17 19:53:32 -0600

    Add tests to the TestsFromStudent file.

--- src/test/java/edu/grinnell/csc207/TestsFromStudent.java
+++ src/test/java/edu/grinnell/csc207/TestsFromStudent.java
@@ -1,14 +1,8 @@
 package edu.grinnell.csc207;
 
 import edu.grinnell.csc207.util.AssociativeArray;
-import edu.grinnell.csc207.util.NullKeyException;
-import edu.grinnell.csc207.util.KeyNotFoundException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
@@ -16,13 +10,86 @@ import org.junit.jupiter.api.Test;
 /**
  * A place for you to put your own tests (beyond the shared repo).
  *
- * @author Your Name Here
+ * @author Sarah Deschamps
  */
 public class TestsFromStudent {
   /**
-   * A simple test.
+   * Checks whether adding a new value with the same key replaces the original value.
+   * @throws Exception
    */
   @Test
-  public void alwaysPass() throws Exception {
-  } // alwaysPass()
+  public void deschampsSarahTest1() throws Exception {
+    AssociativeArray<String, String> arr = new AssociativeArray<String, String>();
+    // Add a bunch of values
+    arr.set("The", "Cranberries");
+    arr.set("Gillian", "Hills");
+    arr.set("Elton", "John");
+    try {
+      assertEquals("Cranberries", arr.get("The"));
+      assertEquals("Hills", arr.get("Gillian"));
+      assertEquals("John", arr.get("Elton"));
+    } catch (Exception e) {
+      fail("Array does not contain expected value deschampsSarahTest1");
+    } // try-catch
+    arr.set("The", "Bangles");
+    try {
+      assertEquals("Bangles", arr.get("The"));
+    } catch (Exception e) {
+      fail("Array does not contain expected value deschampsSarahTest1");
+    } // try-catch
+  } // deschampsSarahTest1()
+
+  /**
+   * When replacing a value, it does not just add a new value.
+   * @throws Exception
+   */
+  @Test
+  public void deschampsSarahTest2() throws Exception {
+    AssociativeArray<String, String> arr = new AssociativeArray<String, String>();
+    // Add a bunch of values
+    arr.set("The", "Cranberries");
+    arr.set("Gillian", "Hills");
+    arr.set("Elton", "John");
+    arr.set("The", "Bangles");
+    try {
+      assertEquals(3, arr.size());
+    } catch (Exception e) {
+      fail("Array is not expected length deschampsSarahTest2");
+    } // try-catch
+  } // deschampsSarahTest2()
+
+  /**
+   * Remove changes the size of the array instead of setting each value to null or empty.
+   * @throws Exception
+   */
+  @Test
+  public void deschampsSarahEdge1() throws Exception {
+    AssociativeArray<String, String> arr = new AssociativeArray<String, String>();
+    // Add a bunch of values
+    arr.set("The", "Cranberries");
+    arr.set("Gillian", "Hills");
+    arr.set("Elton", "John");
+    arr.remove("The");
+    try {
+      assertEquals(2, arr.size());
+    } catch (Exception e) {
+      fail("Remove does not change length deschampsSarahEdge1");
+    } // try-catch
+  } // deschampsSarahEdge1()
+
+  /**
+   * Checks whether set can handle an empty value
+   * @throws Exception
+   */
+  @Test
+  public void deschampsSarahEdge2() throws Exception {
+    AssociativeArray<String, String> arr = new AssociativeArray<String, String>();
+
+    // Add a null value
+    try {
+      arr.set("key", "");
+    } catch (Exception e) {
+      fail("Set cannot handle an empty value");
+    } // try-catch
+  } // deschampsSarahEdge2()
 } // class TestsFromSam
